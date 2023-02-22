826d3e130efa7e0c6574bdbfcf39e69adc11fcfe
pratul <pratul.sharma@gmail.com>
2023-02-21 21:17:25 -0800

    new change

---
+++ user/handler.js
@@ -0,0 +1,90 @@
+
+const AWS = require('aws-sdk')
+
+const cognito = new AWS.CognitoIdentityServiceProvider()
+
+const sendResponse = (statusCode, body) => {
+    const response = {
+        statusCode: statusCode,
+        body: JSON.stringify(body),
+        headers: {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Credentials': true
+        }
+    }
+    return response
+}
+
+const validateInput = (data) => {
+    const body = JSON.parse(data);
+    const { email, password } = body
+    if (!email || !password || password.length < 6)
+        return false
+    return true
+}
+module.exports.loginUser = async (event) => {
+    try {
+        const isValid = validateInput(event.body)
+        if (!isValid)
+            return sendResponse(400, { message: 'Invalid input' })
+
+        const { email, password } = JSON.parse(event.body)
+        const { user_pool_id, client_id } = process.env
+        const params = {
+            AuthFlow: "ADMIN_NO_SRP_AUTH",
+            UserPoolId: user_pool_id,
+            ClientId: client_id,
+            AuthParameters: {
+                USERNAME: email,
+                PASSWORD: password
+            }
+        }
+        const response = await cognito.adminInitiateAuth(params).promise();
+        return sendResponse(200, { message: 'Success', token: response.AuthenticationResult.IdToken })
+    }
+    catch (error) {
+        const message = error.message ? error.message : 'Internal server error'
+        return sendResponse(500, { message })
+    }
+}
+
+module.exports.signupUser = async (event) => {
+    try {
+        const isValid = validateInput(event.body)
+        if (!isValid)
+            return sendResponse(400, { message: 'Invalid input' })
+
+        const { email, password } = JSON.parse(event.body)
+        const { user_pool_id } = process.env
+        const params = {
+            UserPoolId: user_pool_id,
+            Username: email,
+            UserAttributes: [
+                {
+                    Name: 'email',
+                    Value: email
+                },
+                {
+                    Name: 'email_verified',
+                    Value: 'true'
+                }],
+            MessageAction: 'SUPPRESS'
+        }
+        const response = await cognito.adminCreateUser(params).promise();
+        if (response.User) {
+            const paramsForSetPass = {
+                Password: password,
+                UserPoolId: user_pool_id,
+                Username: email,
+                Permanent: true
+            };
+            await cognito.adminSetUserPassword(paramsForSetPass).promise()
+        }
+        return sendResponse(200, { message: 'User registration successful' })
+    }
+    catch (error) {
+        const message = error.message ? error.message : 'Internal server error'
+        return sendResponse(500, { message })
+    }
+}
