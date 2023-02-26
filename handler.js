68a558be0f3a5197147dc1d231a819438c80704c
pratulsharma <pratul.sharma@arm.com>
2023-02-26 14:43:23 -0800

    Add files via upload

--- handler.js
+++ handler.js
@@ -1,6 +1,8 @@
 'use strict';
 const {v4: uuid} = require('uuid');
-const AWS = require('aws-sdk')
+const AWS = require('aws-sdk');
+const stripe = require("stripe")("sk_test_yojwH4SOAP5pcBZLv53QtfpV")
+
 // import AWS, { OpenSearch } from 'aws-sdk'
 
 const dynamodb = new AWS.DynamoDB.DocumentClient();
@@ -10,7 +12,7 @@ module.exports.createDriver = async (event) => {
   const now = new Date();
   const driverId = uuid();
   console.log(event.body);
-  const {driverName, driverMobile, driverDL, driverCar, driverAddress, carType } = JSON.parse(event.body)
+  const {driverName, driverMobile,driverEmail, driverDL, driverCar, driverAddress, account_id, carType } = JSON.parse(event.body)
   console.log(event.body);
 const driver = {
   driverName,
@@ -19,6 +21,7 @@ const driver = {
    driverDL, 
    driverCar, 
    driverAddress, 
+   account_id,
    carType,
   createdAt: now.toISOString()
 }
@@ -43,10 +46,67 @@ catch(error){
 
 
 
+module.exports.driverPaymentOnboarding = async (event) => {
+  const now = new Date();
+  const dponbId = uuid();
+  console.log(event.body);
+  //check if the driver exixts in the db - based on the mobile number
+  //create stripe account 
+  //create the account onboarding link 
+  // prefill the link 
+  // send the link to the driver to setup his account with the platform 
+  const {driverMobile,driverEmail } = JSON.parse(event.body)
+  console.log(event.body);
+const driver = {
+   driverMobile, 
+   driverEmail,
+  createdAt: now.toISOString()
+}
+try{
+
+// Get the Driver Info
+  const params = {
+    TableName: 'driverTable',
+    Key: { driverMobile},
+    FilterExpression: "attribute_not_exists(account_id) or account_id = :null",
+    ExpressionAttributeValues: {
+              ':null': null
+
+    },
+  };
+
+//  const driverAccountIdExists = await dynamodb.query(params).promise()
+
+//  console.log("DRIVER ACCOUNT ID EXISTS?", driverAccountIdExists);
+// check if the account_id for this driver already exists?
+// if(driverAccountIdExists.account_id == 'null'){
+// call stripe to create the account_id 
+  console.log("Driver account ID does not exist")
+  const account = await stripe.accounts.create({type: "express"});
+  console.log("STRIPE ACCOUNT ID", account.id)
+// save the account id in the driverTable 
+
+
+// }
+
+
+}
+catch(error){
+  console.error(error)
+}
+  return {
+    statusCode: 200,
+    body: JSON.stringify(
+       driver 
+    ),
+  };
+};
+
+
  module.exports.createRide = async (event) => {
   const now = new Date();
   const rideId = uuid();
-  const {fromCity, toCity, date_ride, time_ride, maxP, carType, price_ride, status_ride, driverMobile} = JSON.parse(event.body)
+  const {fromCity, toCity, date_ride, time_ride, maxP, carType, price_ride, status_ride, driverMobile, driverEmail} = JSON.parse(event.body)
 const ride = {
   rideId,
   fromCity,
@@ -58,6 +118,7 @@ const ride = {
   price_ride,
   status_ride,
   driverMobile,
+  driverEmail,
   createdAt: now.toISOString()
 }
 try{
@@ -79,10 +140,11 @@ catch(error){
 
 module.exports.bookRide = async (event) => {
   const now = new Date();
-  const bookId = uuid();
-  const {fromCity, toCity, date_ride, time_ride, maxP, carType, price_ride, status_ride, driverMobile, driverEmail} = JSON.parse(event.body)
-const ride = {
-  bookId,
+  const bookRideId = uuid();
+  console.log("RESOLVE))))))))))))))))))))))))))))))))))))))))", event.body)
+  const {fromCity, toCity, date_ride, time_ride, maxP, carType, price_ride,  driverMobile, driverEmail} = JSON.parse(event.body)
+const bookRide = {
+  bookRideId,
   fromCity,
   toCity,
   date_ride,
@@ -90,25 +152,41 @@ const ride = {
   maxP,
   carType,
   price_ride,
-  status_ride,
   driverMobile,
+  driverEmail,
   createdAt: now.toISOString()
 }
 try{
 // Get the Driver Info
-  await dynamodb.put({
-    TableName:'ridesTable',
-    Item: ride
-  }).promise();
+  const params = {
+    TableName: 'driverTable',
+    KeyConditionExpression: '#driverMobile = :driverMobile',
+    ExpressionAttributeValues: {
+      ':driverMobile': driverMobile,
+    },
+    ExpressionAttributeNames: {
+      '#driverMobile': 'driverMobile',
+    },
+  };
+
+ const driverExists = await dynamodb.query(params).promise()
+ if(driverExists.Items != null){
+
+  // so we got the driver information; we need to create the payment and send the 
+  // checkout form to the FE 
+  //
+  await dynamodb.put(
+
+
+  ).promise()
+ }
 }
 catch(error){
   console.error(error)
 }
   return {
     statusCode: 200,
-    body: JSON.stringify(
-       ride 
-    ),
+    "message" : "it went fine"
   };
 };
 
